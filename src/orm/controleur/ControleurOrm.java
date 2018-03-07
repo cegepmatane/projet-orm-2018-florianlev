bb6e55b0ec3f04b07ba7078afa029cab53aeaf43
gui45 <guillaume.d.marceau@gmail.com>
2018-03-06 21:29:06 -0500

    ajout de la base de la vue

--- src/orm/controleur/ControleurOrm.java
+++ src/orm/controleur/ControleurOrm.java
@@ -1,4 +1,11 @@
 package orm.controleur;
 
+import orm.vue.VueOrm;
+
 public class ControleurOrm {
+	VueOrm vue;
+	public ControleurOrm(VueOrm vue)
+	{
+		this.vue = vue;
+	}
 }
--- src/orm/vue/VueOrm.java
+++ src/orm/vue/VueOrm.java
@@ -1,4 +1,42 @@
 package orm.vue;
 
-public class VueOrm {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import orm.controleur.ControleurOrm;
+import orm.modele.Planete;
+
+public class VueOrm extends Application{
+	
+	protected ControleurOrm controleur;
+	protected StackPane racine;
+	protected Alert alerte;
+	protected Alert alerteContinent;
+	protected Stage scenePrincipale; 
+	protected Planete planete;
+	public VueOrm()
+	{
+		controleur = new ControleurOrm(this);
+		planete = new Planete();
+		planete.setNom("terre");
+	}
+	
+	@Override
+	public void start(Stage scenePrincipale) throws Exception 
+	{
+		this.scenePrincipale = scenePrincipale;
+		Label label = new Label(planete.getNom());
+		StackPane racine = new StackPane();
+		
+		racine.getChildren().add(label);
+		Scene scene = new Scene(racine, 500, 500);
+		
+		scenePrincipale.setScene(scene);
+		scenePrincipale.setTitle("Planet");
+		scenePrincipale.show();
+	}
+
 }
