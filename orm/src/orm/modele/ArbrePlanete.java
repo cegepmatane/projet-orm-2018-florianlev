534f7f6affc3b4d77b8bc412c0c09b9339cd6b17
gui45 <guillaume.d.marceau@gmail.com>
2018-03-26 23:03:10 -0400

    ajout de l'arbre et de la fonction suprimer de l'arbre

---
+++ orm/src/orm/modele/ArbrePlanete.java
@@ -0,0 +1,84 @@
+package orm.modele;
+
+public class ArbrePlanete {
+	
+	protected Noeud noeud;
+	class Noeud
+	{
+		protected Planete planete;
+		protected Noeud gauche;
+		protected Noeud droite;
+		protected int valeur;
+		public Noeud(Planete planete, int valeur)
+		{
+			this.planete = planete;
+			this.valeur = valeur;
+		}
+	}
+	public ArbrePlanete()
+	{
+		noeud = null;
+	}
+	public void ajouter(Planete planete)
+	{
+		Noeud nouveau = new Noeud(planete, recupererValeur(planete));
+		if(null == noeud)
+		{
+			noeud = new Noeud(planete, recupererValeur(planete));
+		}
+		else
+		{
+			ajouter(noeud, nouveau);
+		}
+	}
+	private void ajouter(Noeud courrant, Noeud nouveau)
+	{
+		if(nouveau.valeur > courrant.valeur)
+		{
+			if(null == courrant.droite)
+			{
+				courrant.droite = nouveau;
+			}else
+			{
+				ajouter(courrant.droite, nouveau);
+			}
+		}else
+		{
+			if(null == courrant.gauche)
+			{
+				courrant.gauche = nouveau;
+			}else
+			{
+				ajouter(courrant.gauche, nouveau);
+			}
+		}
+	}
+	public void afficher()
+	{
+		afficher(this.noeud);
+	}
+	protected void afficher(Noeud noeud)
+	{
+		if (null == noeud) 
+			return;
+        if (null != noeud.gauche)
+        	afficher(noeud.gauche);
+        System.out.println(noeud.planete.getNom());
+        if (null != noeud.droite) 
+        	afficher(noeud.droite);
+	}
+	private int recupererValeur(Planete planete)
+	{
+		int valeur = 0;
+		valeur += Integer.valueOf(planete.getNom().charAt(0));
+		valeur += Integer.valueOf(planete.getNom().charAt(1));
+		valeur += Integer.valueOf(planete.getNom().charAt(2));
+		valeur += Integer.valueOf(planete.getNom().charAt(3));
+		valeur += Integer.valueOf(planete.getNom().charAt(4));
+		valeur += planete.getNom().length();
+		
+		System.out.println(valeur);
+		
+		return valeur;
+	}
+}
