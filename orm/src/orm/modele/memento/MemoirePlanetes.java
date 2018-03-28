14b377914a891e081c023f83bff2c04fe4486bfe
Unknown <flolevy33@gmail.com>
2018-03-28 01:54:00 -0400

    ecriture des fonctions d'ajout memo de set dans meoirePlanete

--- orm/src/orm/modele/memento/MemoirePlanetes.java
+++ orm/src/orm/modele/memento/MemoirePlanetes.java
@@ -1,4 +1,29 @@
 package orm.modele.memento;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MemoirePlanetes {
+    protected List<MementoPlanete> listePlanete;
+
+    public MemoirePlanetes()
+    {
+        this.listePlanete = new ArrayList<MementoPlanete>();
+    }
+
+    public List<MementoPlanete> getListePlanete()
+    {
+        return listePlanete;
+    }
+
+    public void ajouterMemento(MementoPlanete memento)
+    {
+        this.listePlanete.add(memento);
+    }
+
+    public void setListePlanete(List<MementoPlanete> listePlanete)
+    {
+        this.listePlanete = listePlanete;
+    }
+
+
 }
