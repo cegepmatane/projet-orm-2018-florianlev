4919af4eb3168d8f2595d97551cb498a9015257a
Unknown <flolevy33@gmail.com>
2018-03-28 01:57:01 -0400

    ecriture fonctions getPlanete set et constructeur dans memento

--- orm/src/orm/modele/memento/MementoPlanete.java
+++ orm/src/orm/modele/memento/MementoPlanete.java
@@ -1,4 +1,26 @@
 package orm.modele.memento;
+import orm.modele.Planete;
+
+import java.util.Date;
 
 public class MementoPlanete {
+
+    protected Planete planete;
+    protected int version;
+    static protected int versionCourante;
+
+    public MementoPlanete(){
+        this.planete = new Planete();
+        this.version = versionCourante++;
+    }
+
+    public Planete getPlanete()
+    {
+        return planete;
+    }
+
+    public void setPlanete(Planete planete)
+    {
+        this.planete = planete;
+    }
 }
